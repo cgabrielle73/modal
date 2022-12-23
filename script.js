0dadf94374cb1a7f3745a45ee18750483694ce22
Clara <clara.gabrielle@youxgroup.com.br>
2022-12-23 15:59:21 -0300

    Adicionando funcionalidade de abrir e fechar modal

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+const btnsShowModal = document.querySelectorAll('.show-modal')
+const btnCloseModal = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnsShowModal.forEach(btn => btn.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
